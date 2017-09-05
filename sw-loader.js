49b46c0d32d66f0c1531166077194c8856acd0be
John Reyes <johnlreyes@yahoo.com>
2017-09-06 02:16:20 +0800

    updated

--- sw-loader.js
+++ sw-loader.js
@@ -1,13 +1,13 @@
 if ('serviceWorker' in navigator) {
-    console.log('Service Worker supported');
+    //console.log('Service Worker supported');
     navigator.serviceWorker
         .register('service-worker.js', {scope: ' '})
         .then(function() {
-            console.log('Service Worker Registered');
+            //console.log('Service Worker Registered');
         })
         .catch(function(err) {
-            console.error('Problem registering service worker: ' + err);
+            //console.error('Problem registering service worker: ' + err);
         });
 } else {
-    console.log('Service Worker not supported');
+    //console.log('Service Worker not supported');
 }
