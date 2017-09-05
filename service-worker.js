e8c9994e384f04016d725f6a15901a85d11af1d4
John Reyes <johnlreyes@yahoo.com>
2017-09-06 00:14:18 +0800

    updated

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,26 @@
 
 var enableLogs = true;
 var cacheName = 'offline';
-var version = '0905171155PM';
+var version = '0906171213PM';
 var cache = cacheName + '-' + version;
 var fileList = [
     'index.html',
     'sw-loader.js',
-    'img/profile.jpg',
-    'css/bootstrap.min.css',
-    'css/main.css',
-    'js/jquery.min.js',
-    'js/bootstrap.min.js',
+    'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.7.0/highlight.min.js',
+    'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+    'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+    'https://maxcdn.bootstrapcdn.com/js/ie10-viewport-bug-workaround.js',
+    'https://maxcdn.bootstrapcdn.com/css/ie10-viewport-bug-workaround.css',
+    'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css',
+    'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/fonts/fontawesome-webfont.woff2?v=4.6.3',
+    'https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,200bold,400old',
+    'https://fonts.gstatic.com/s/sourcesanspro/v10/ODelI1aHBYDBqgeIAH2zlNV_2ngZ8dMf8fLgjYEouxg.woff2',
+    'https://fonts.gstatic.com/s/sourcesanspro/v10/toadOcfmlt9b38dHJxOBGEo0As1BFRXtCDhS66znb_k.woff2',
     'js/docs.min.js',
     'js/main.js',
-    'js/ie10-viewport-bug-workaround.js',
-    'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css',
-    'https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,200bold,400old',
-    'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/fonts/fontawesome-webfont.woff2?v=4.6.3',
-    'https://fonts.gstatic.com/s/sourcesanspro/v10/ODelI1aHBYDBqgeIAH2zlNV_2ngZ8dMf8fLgjYEouxg.woff2',
-    'https://fonts.gstatic.com/s/sourcesanspro/v10/toadOcfmlt9b38dHJxOBGEo0As1BFRXtCDhS66znb_k.woff2'
+    'img/profile.jpg',
+    'css/main.css'
 ];
 
 self.addEventListener('install', function(event) {
